d7ff85dab4a715190c1bfd931b9f8a784e2cf9f8
nmatare <nmatare@chicagobooth.edu>
2017-01-14 09:09:39 -0600

    non-linear function ggplot2 added

--- homework1/homework1.R
+++ homework1/homework1.R
@@ -16,59 +16,69 @@
 # Question 1
 ########
 
-	doQuestionOne <- function(equation){
+	makeSimulation <- function(equation, noise = 0){
+					# noise controls the number of superfurlous predictor vars added to dataset
 
-					makeSimulation <- function(equation){
+					# create training dataset
+					x <- rnorm(100, mean = 0, sd = 1) #predictor x
+					x.noise <- NULL					
+					if(noise > 0) x.noise <- as.data.frame(sapply(rep(100, noise), function(w) rnorm(w, mean = 0, sd = 1))) # generate noise variables from normal distribution
 
-									# create training dataset
-									x <- rnorm(100, mean = 0, sd = 1)
-									y <- eval(equation) + rnorm(100, mean = 0, sd = 1) #y is the function plust some noise
-									train <- cbind.data.frame(y, x)
-						
-									test <- list() # create testing dataset
-									for(i in 1:100){ #do 100 times to get 10,000 observations
+					e <- rnorm(100, mean = 0, sd = 1) # random noise
+					y <- eval(equation) + e # y is the function plus some noise
 
-										test[[i]] <- cbind(	y = eval(equation) + rnorm(100, mean = 0, sd = 1), # why is the function plus noise
-															x =  rnorm(100, mean = 0, sd = 1) # x is from norm(0,1)
-														)
-									}
+					train <- as.data.frame(cbind(y, x, x.noise)) #train models on predictor and noise
+		
+					test <- list() # create testing dataset
+					for(i in 1:100){ #do 100 times to get 10,000 observations
 
-									test <- as.data.frame(do.call(rbind, test))
-									stopifnot(isTRUE(dim(test)[1] == 10000)) # sanity check
-									return(list(train = train, test = test))
+						x <- rnorm(100, mean = 0, sd = 1)
+						e <- rnorm(100, mean = 0, sd = 1) # random noise
+						y <- eval(equation) + e
+						test[[i]] <- cbind.data.frame(y = y, x = x) # true model has no noise so don't include it in the test set
 					}
 
+					test <- as.data.frame(do.call(rbind, test))
+					stopifnot(isTRUE(dim(test)[1] == 10000)) # sanity check
+					return(list(train = train, test = test))
+	}
+
+	makeKnn <- function(data, K){ #wrapper function for Knn
+					knn <- kknn(		formula = y ~ ., # where . is everything or x or x1, x2, x3, etc
+										train = data$train, # train on relationship between x and y
+										test = data$test, # given x, find y 
+										kernel = "rectangular", 
+										k = K)
+					return(knn$fitted.values)
+	}
+
+	doQuestionOne <- function(equation, p1 = TRUE, p2 = TRUE, p3 = TRUE, ...){
+
+					#p1/p3 control verbosity of print
 					simulation <- makeSimulation(equation)
 
 					# Part 2 and 3
 					base.plot <- ggplot(	data = simulation$test, aes(x = x, y = y)) + 
 											geom_point(color = "darkgrey", size = 1, alpha = 3 / 5) + # show the relationship
-											geom_abline(intercept = 2, slope = 1.8, color = "black") + # show the true linear equation
+											stat_function(fun = function(x) eval(equation), colour = "black") + # show the true linear equation
 											geom_text(aes(x = max(x) - 1, y = 8, label = 'True Function', sep = ""), vjust = -4, size = 4, color = "black") # add text
+					plot(base.plot)
+
 
 					plot1 <- 	base.plot + ggtitle("Linear Model")	+		
 								geom_smooth(method = "lm", col = 'blue', linetype = "dashed", show.legend = TRUE) + # show the true line; this is essentially lm(y ~ x, data = simulation)
 								geom_text(aes(x = max(x) - 1, y = 8, label = 'Linear Fit', sep = ""), vjust = 4, size = 4, color = "blue")
 
-					print(plot1); dev.new()
+					if(p1) print(plot1); dev.new()
 
 					# Part 4
-					makeKnn <- function(data, K){ #wrapper function for Knn
-									knn <- kknn(		formula = y ~ x, 
-														train = data$train, # train on relationship between x and y
-														test = data$test, # given x, find y 
-														kernel = "rectangular", 
-														k = K)
-									return(knn$fitted.values)
-					}
-
 					knn2fitted <- 	cbind.data.frame(fit = makeKnn(K = 2, data = simulation), x = simulation$test$x) #fit on training data and then output the fitted y values and corresponding x's
 					knn12fitted <- 	cbind.data.frame(fit = makeKnn(K = 12, data = simulation), x = simulation$test$x)
 
 					plot.knn2 <- base.plot + geom_line(data = knn2fitted, aes(y = fit, x = x), col = "green") + ggtitle("KNN at K = 2")
 					plot.knn12 <- base.plot + geom_line(data = knn12fitted, aes(y = fit, x = x), col = "blue") + ggtitle("KNN at K = 12")
 
-					plot2 <- grid.arrange(plot.knn2, plot.knn12, ncol = 2); dev.new()
+					if(p2) plot2 <- grid.arrange(plot.knn2, plot.knn12, ncol = 2); dev.new()
 
 					# Part 5
 					MSEs <- list()
@@ -87,40 +97,37 @@
 											xlab("-log(1/K)") +
 											ggtitle("MSEs of KNNs") 
 									
-					print(plot3)
+					if(p3) print(plot3)
 	}
 
 	########
 	# Simulation 1
 	########
 
-	doQuestionOne(quote(2 + 1.8 * x))
+	doQuestionOne(equation = quote(2 + 1.8 * x), noise = 0)
 
 	########
 	# Simulation 2
 	########
 
-	doQuestionOne(quote(3 + exp(x + 1) + e))
+	doQuestionOne(quote(3 + exp(x + 1)))
 
 	########
 	# Simulation 3
 	########
 
-	doQuestionOne(quote(2 + sin(2 * x) + e))
+	doQuestionOne(quote(2 + sin(2 * x)))
 
 	########
 	# Simulation 4
 	########
 
-	## Special case where X grows, will have to write custom function
+	grid.arrange(nrow = 4, ncol = 5)
+	for(n in 1:20){
 
-	doQuestionOne()
+		doQuestionOne(quote(2 + sin(2 * x)), noise = 20, p1 = FALSE, p2 = FALSE) # only print MSE charts
 
-	z * rnorm(1, mean = 0, sd = 0)
+		draw_plot(plot, x = 0, y = 0, width = 1, height = 1)
+	}
 
-	2 + sin(2 * x) +  + e
 
-	quote(sin(2 * x) + 2 + 0 * rnorm(1, mean = 0, sd = 0) + e)
-
-	sin(2 * x) + 2 + 0 * rnorm(1, mean = 0, sd = 0) + e
-	yi = sin(2xi1) + 2 + 0 × xi2 + · · · + 0 × xip + i
