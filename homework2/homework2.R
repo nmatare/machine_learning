1bb6504e19c9d915f0a8c26b235f278a38869a7e
nmatare <nmatare@chicagobooth.edu>
2017-01-27 01:19:41 -0600

    q1 models added

--- homework2/homework2.R
+++ homework2/homework2.R
@@ -8,11 +8,13 @@
 ########
 
 	options("width" = 250)
-	options(scipen = 999)
-	options(digits = 3)
+	options(scipen  = 999)
+	options(digits  = 003)
 
-	library(ggplot2); require(gridExtra); library(MASS); library(Matrix)
+	library(ggplot2); require(gridExtra); library(MASS); library(Matrix); library(parallel)
 	library(kknn); library(boot); library(rpart); library(data.table)
+	library(gamlr); library(BayesTree); library(xgboost); library(ranger)
+
 	set.seed(666) # the devils seed
 
 	xnaref <- function(x){
@@ -41,8 +43,8 @@
 	bike.train <- as.data.table(read.csv(url("https://raw.githubusercontent.com/ChicagoBoothML/ML2016/master/hw02/Bike_train.csv")))
 	bike.test <- as.data.table(read.csv(url("https://raw.githubusercontent.com/ChicagoBoothML/ML2016/master/hw02/Bike_test.csv")))
 	
-	bike.train[,UID := .I]; setkey(bike.train, UID) # create UID column
-	bike.test[,UID := .I]; setkey(bike.test, UID) # create UID column
+	bike.train[ ,UID := .I]; setkey(bike.train, UID) # create UID column
+	bike.test[ ,UID := .I]; setkey(bike.test, UID) # create UID column
 	bike.test[,count := NA] # these are to be predicted
 
 	data <- rbind(bike.train, bike.test)	
@@ -62,8 +64,8 @@
 	dummies <- model.matrix( ~ year + month + day + hour + season + holiday + workingday + weather + daylabel, data = data)[,-1]
 	all.data <- cbind(data[ ,c("count", "temp", "atemp", "humidity", "windspeed"), with = FALSE], dummies)
 
-	trainX <- Matrix(as.matrix(all.data[!is.na(count), !('count'), with = FALSE]))
-	trainY <- all.data[!is.na(count),  ('count'), with = FALSE]
+	trainX <- all.data[!is.na(count), !('count'), with = FALSE]
+	trainY <- all.data[!is.na(count), ('count'), with = FALSE]
 
 	testX <- Matrix(as.matrix(all.data[is.na(count), !('count'), with = FALSE]))
 	testY <- NULL
@@ -72,8 +74,24 @@
 	# Build Models
 	########
 
-	## TO DO
+	LASSO <- cv.gamlr(trainX, trainY, family = 'gaussian', verb = TRUE, nfold = 10)
+	sqrt(min(LASSO$cvm)) # RMSE 
 
+	XGBST <- xgboost(data = as.matrix(trainX), label = as.vector(unlist(trainY)), max_depth = 2, nthread = detectCores() - 1, nrounds = 100, verbose = 1, objective = "reg:linear")
+
+	RF <- ranger(count ~., 	data = cbind.data.frame(trainY, trainX), probability = FALSE, classification = FALSE, num.trees = 5000, write.forest = TRUE, 
+							num.threads = detectCores() - 1, importance = 'impurity', verbose = TRUE
+				)
+
+	sqrt(RF$prediction.error) # RMSE
+
+	BART <- bart(	x.train = as.data.frame(trainX), 
+					y.train = as.double(unlist(trainY)), 
+					x.test = as.data.frame(trainX),
+					ntree = 500, ndpost = 200, nskip = 100, verbose = TRUE
+				)
+
+	sqrt(mean((trainY - BART$yhat.test.mean) ^ 2)) # BART$sigest
 
 ########
 # Question 2
